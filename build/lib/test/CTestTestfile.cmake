5da7bb60d17a6fb33297f665faccb7c79bab979c
mdasari823 <mdasari@cs.stonybrook.edu>
2018-06-25 22:06:18 +0000

    cleaning and removing build

--- build/lib/test/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/vEPC/srsLTE/lib/test
-# Build directory: /home/ubuntu/vEPC/srsLTE/build/lib/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(asn1)
-subdirs(common)
-subdirs(upper)
--- build/srsepc/src/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/vEPC/srsLTE/srsepc/src
-# Build directory: /home/ubuntu/vEPC/srsLTE/build/srsepc/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(mme)
-subdirs(hss)
-subdirs(spgw)
